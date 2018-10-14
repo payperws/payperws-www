1b173dd3d087589416f0dd3a7eab27f02a56a23a
Vlad Suciu <suciu.vlad@gmail.com>
2018-10-14 11:35:29 +0300

    Add styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,20 @@
-import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-// Document component is strongly typed with `@types/next`
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
-      <html lang="en">
+      <html>
+        <Head>{this.props.styleTags}</Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,7 @@
-export default () => <div>Welcome to next.js!</div>
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  color: red;
+`;
+
+export default () => <Heading>Welcome to next.js!</Heading>;
